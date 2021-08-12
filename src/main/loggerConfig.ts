bd17500c48b73a7dd0d0a4865d2f173fe90314e7
jay-nanduri <72423211+jay-nanduri@users.noreply.github.com>
2021-08-12 04:11:03 -0400

    Fix logger config (#7)
    * updated logger config
    
    * updated main.yml for integration tests
    
    * branch change

--- src/main/loggerConfig.ts
+++ src/main/loggerConfig.ts
@@ -3,18 +3,10 @@ import { configure, getLogger } from 'log4js';
 // appenders
 configure({
   appenders: {
-    console: { type: 'stdout', layout: { type: 'colored' } },
-    dateFile: {
-      type: 'dateFile',
-      filename: `cxAST.log`,
-      layout: { type: 'basic' },
-      compress: true,
-      daysToKeep: 14,
-      keepFileExt: true
-    }
-  },
-  categories: {
-    default: { appenders: ['console', 'dateFile'], level: "info" }
+    console: { type: 'stdout', layout: { type: "messagePassThrough" } },
+},
+categories: {
+    default: { appenders: ['console'], level: "info" }
   }
 });
 
