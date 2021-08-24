1dd876edfc5120dd96fa47698695328b9eebaa57
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2021-08-24 10:20:49 +0100

    test

--- src/main/ExecutionService.ts
+++ src/main/ExecutionService.ts
@@ -42,7 +42,7 @@ export class ExecutionService {
                 });
             cp.stdout.on('data', (data: any) => {
                 if (data) {
-                    logger.info(`${data}`);
+                    logger.info(`test ${data}`);
                     if (isJsonString(data.toString())) {
                         let resultObject = JSON.parse(data.toString().split('\n')[0]);
                         if (resultObject instanceof Array) {
