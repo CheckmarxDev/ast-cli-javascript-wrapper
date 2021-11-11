22175f5a2714a6b3ca5138464fe913e4db27031f
tiagobcx <tiago.baptista@checkmarx.com>
2021-11-11 17:08:37 +0000

    fix tests increase timeout

--- src/tests/ScanTest.test.ts
+++ src/tests/ScanTest.test.ts
@@ -11,6 +11,7 @@ describe("ScanCreate cases",() => {
         params.set(CxParamType.S, "./src");
         params.set(CxParamType.FILTER, "*.ts,!**/node_modules/**/*");
         params.set(CxParamType.BRANCH, "master");
+
         const auth = new CxWrapper(cxScanConfig);
         const data = await auth.scanCreate(params);
         const cxCommandOutput: CxCommandOutput = data;
@@ -20,39 +21,39 @@ describe("ScanCreate cases",() => {
         expect(scanShowObject.payload.pop().Status).toEqual("Completed");
     })
 
-    it('ScanCreate Failure case', async () => {
-        const params = new Map();
-        params.set(CxParamType.PROJECT_NAME, "ast-cli-javascript-integration-failure");
-        params.set(CxParamType.S, "./src");
-        params.set(CxParamType.SAST_PRESET_NAME, "Checkmarx Default Fake");
-        params.set(CxParamType.BRANCH, "master");
-        const auth = new CxWrapper(cxScanConfig);
-        const cxCommandOutput: CxCommandOutput = await auth.scanCreate(params);
-        const scanObject = cxCommandOutput.payload.pop();
-        const scanShowObject = await auth.scanShow(scanObject.ID);
-        console.log(" Json object from failure case: " + JSON.stringify(scanShowObject));
-        expect(scanShowObject.payload.pop().Status).toEqual("Failed");
-    })
-
-    it('ScanList Successful case', async () => {
-        const auth = new CxWrapper(cxScanConfig);
-        const cxCommandOutput: CxCommandOutput = await auth.scanList("");
-        expect(cxCommandOutput.payload.length).toBeGreaterThan(0);
-    });
-
     it('ScanCreate Successful case with Branch', async () => {
         const params = new Map();
         params.set(CxParamType.PROJECT_NAME, "ast-cli-javascript-integration-success-branch");
         params.set(CxParamType.S, "./src");
         params.set(CxParamType.FILTER, "*.ts,!**/node_modules/**/*");
         params.set(CxParamType.BRANCH, "master");
+
         const auth = new CxWrapper(cxScanConfig);
-        const cxCommandOutput: CxCommandOutput = await auth.scanCreate(params);
+
+        const data = await auth.scanCreate(params);
+        const cxCommandOutput: CxCommandOutput = data;
         const scanObject = cxCommandOutput.payload.pop();
         const scanShowObject = await auth.scanShow(scanObject.ID);
         console.log(" Json object from successful wait mode case with branch: " +JSON.stringify(scanShowObject));
         expect(scanShowObject.payload.pop().Status).toEqual("Completed");
 
+    })
+
+    it('ScanCreate Failure case', async () => {
+        const params = new Map();
+        params.set(CxParamType.PROJECT_NAME, "ast-cli-javascript-integration-failure");
+        params.set(CxParamType.S, "./src");
+        params.set(CxParamType.SAST_PRESET_NAME, "Checkmarx Default Fake");
+        params.set(CxParamType.BRANCH, "master");
+
+        const auth = new CxWrapper(cxScanConfig);
+
+        const data = await auth.scanCreate(params);
+        const cxCommandOutput: CxCommandOutput = data;
+        const scanObject = cxCommandOutput.payload.pop();
+        const scanShowObject = await auth.scanShow(scanObject.ID);
+        console.log(" Json object from failure case: " + JSON.stringify(scanShowObject));
+        expect(scanShowObject.payload.pop().Status).toEqual("Failed");
     })
 
     it('ScanCreate Successful case no wait mode', async () => {
@@ -62,11 +63,21 @@ describe("ScanCreate cases",() => {
         params.set(CxParamType.SAST_PRESET_NAME, "Checkmarx Default Fake");
         params.set(CxParamType.ADDITIONAL_PARAMETERS, "--async");
         params.set(CxParamType.BRANCH, "master");
+
         const auth = new CxWrapper(cxScanConfig);
-        const cxCommandOutput: CxCommandOutput = await auth.scanCreate(params);
+
+        const data = await auth.scanCreate(params);
+        const cxCommandOutput: CxCommandOutput = data;
         const scanObject = cxCommandOutput.payload.pop();
         const scanShowObject = await auth.scanShow(scanObject.ID);
-        console.log(" Json object from successful no wait mode case: " + JSON.stringify(scanShowObject));
-        expect(scanShowObject.payload.pop().Status).toEqual("Running");
+        console.log(" Json object from successful no wait mode case: " + JSON.stringify(scanShowObject))
+        expect(scanShowObject.payload.pop().Status).toEqual("Running")
     })
+
+    it('ScanList Successful case', async () => {
+        const auth = new CxWrapper(cxScanConfig);
+        const data = await auth.scanList("");
+        const cxCommandOutput: CxCommandOutput = data;
+        expect(cxCommandOutput.payload.length).toBeGreaterThan(0);
+    });
 });
