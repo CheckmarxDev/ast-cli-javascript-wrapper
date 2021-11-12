70790bc43e6ddc958cbe5ef1969059fc5cb416f4
tiagobcx <tiago.baptista@checkmarx.com>
2021-11-12 17:05:56 +0000

    fix some structure issues

--- src/main/wrapper/CxConfig.ts
+++ src/main/wrapper/CxConfig.ts
@@ -1,8 +1,8 @@
 export class CxConfig {
-    baseUri: string = undefined;
-    pathToExecutable: string = undefined;
-    clientId: string = undefined;
-    clientSecret: string = undefined;
-    apiKey: string = undefined;
-    tenant:string =undefined;
+    baseUri: string = "";
+    pathToExecutable: string = "";
+    clientId: string = "";
+    clientSecret: string = "";
+    apiKey: string = "";
+    tenant:string ="";
 }
