cf808fc42b2a8af6865c7e8f77743c4e7319a4e1
AndreGCX <andre.goncalves@checkmarx.com>
2022-03-23 18:16:25 +0000

    separating logger configurations

--- src/main/wrapper/loggerConfig.ts
+++ src/main/wrapper/loggerConfig.ts
@@ -1,28 +1,30 @@
 import { configure, getLogger } from 'log4js';
 
-export function getLoggerWithFilePath(logFilePath ?: string) {
+function configurationWithFile(logFilePath: string) {
+	return configure({
+		appenders: {
+			file: { type: 'fileSync', filename: logFilePath },
+			console: { type: 'stdout', layout: { type: "messagePassThrough" } },
+		},
+		categories: {
+			default: { appenders: ['console', 'file'], level: "info" }
+		}
+	});
+}
 
-  if(logFilePath) {
-    configure({
-      appenders: {
-        file: { type: 'fileSync', filename: logFilePath ? logFilePath : "" },
-        console: { type: 'stdout', layout: { type: "messagePassThrough" } },
-    },
-    categories: {
-        default: { appenders: ['console', 'file'], level: "info" }
-      }
-    });
+function configurationWithoutFile() {
+	return configure({
+		appenders: {
+			console: { type: 'stdout', layout: { type: "messagePassThrough" } },
+		},
+		categories: {
+			default: { appenders: ['console',], level: "info" }
+		}
+	});
+}
 
-  } else {
-    configure({
-      appenders: {
-        console: { type: 'stdout', layout: { type: "messagePassThrough" } },
-    },
-    categories: {
-        default: { appenders: ['console'], level: "info" }
-      }
-    });
-  }
-} 
+export function getLoggerWithFilePath(logFilePath?: string) {
+	logFilePath ? configurationWithFile(logFilePath) : configurationWithoutFile()
+}
 
 export const logger = getLogger();
