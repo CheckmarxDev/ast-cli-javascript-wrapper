32caafc91f58f61ab6670ade45e97b466f5ad704
Pedro Lopes <83576881+pedrompflopes@users.noreply.github.com>
2022-04-08 22:05:45 +0100

    Update update_cli.sh

--- .github/scripts/update_cli.sh
+++ .github/scripts/update_cli.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 release=$1
-filename_windows=ast-cli_${release}_windows_x64.zip
+filename_windows=ast-cli_${release}_windows_x64.tar.gz
 filename_linux=ast-cli_${release}_linux_x64.tar.gz
 filename_darwin=ast-cli_${release}_darwin_amd64.tar.gz
 
 #Windows
 echo "Updating windows binary"
 wget https://github.com/Checkmarx/ast-cli/releases/download/${release}/${filename_windows}
-unzip ${filename_windows} -d tmp
+tar -xvzf ${filename_windows} -d tmp
 mv ./tmp/cx.exe ./src/main/wrapper/resources/cx.exe
 rm -r tmp
 rm ${filename_windows}
