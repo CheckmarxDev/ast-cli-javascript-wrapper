9d668aabf624f586514952cb942c8da832fbcac1
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2022-06-09 18:01:03 +0100

    test

--- src/tests/ProjectTest.test.ts
+++ src/tests/ProjectTest.test.ts
@@ -1,44 +1,41 @@
-import {CxWrapper} from '../main/wrapper/CxWrapper';
-import {CxCommandOutput} from "../main/wrapper/CxCommandOutput";
-import {BaseTest} from "./BaseTest";
-import {CxParamType} from "../main/wrapper/CxParamType";
+import { CxWrapper } from '../main/wrapper/CxWrapper';
+import { CxCommandOutput } from "../main/wrapper/CxCommandOutput";
+import { BaseTest } from "./BaseTest";
+import { CxParamType } from "../main/wrapper/CxParamType";
 import CxScan from "../main/scan/CxScan";
 
-describe("ProjectList cases",() => {
-    const cxScanConfig = new BaseTest();
-    it('ProjectList Successful case', async () => {
-        const auth = new CxWrapper(cxScanConfig);
-        const data = await auth.projectList("");
-        const cxCommandOutput: CxCommandOutput = data;
-        expect(cxCommandOutput.payload.length).toBeGreaterThan(0);
-    });
+it('ProjectList Successful case', async () => {
+    const auth = new CxWrapper(new BaseTest());
+    const data = await auth.projectList("");
+    const cxCommandOutput: CxCommandOutput = data;
+    expect(cxCommandOutput.payload.length).toBeGreaterThan(0);
+});
 
-    it('ProjectShow Successful case', async () => {
-        const params = new Map();
-        params.set(CxParamType.PROJECT_NAME, "ast-cli-javascript-integration-success");
-        params.set(CxParamType.S, "./src");
-        params.set(CxParamType.FILTER, "*.ts,!**/node_modules/**/*");
-        params.set(CxParamType.BRANCH, "master");
-        const auth = new CxWrapper(cxScanConfig);
-        const cxCommandOutput: CxCommandOutput = await auth.scanCreate(params);
-        const scan: CxScan = cxCommandOutput.payload.pop();
+it('ProjectShow Successful case', async () => {
+    const params = new Map();
+    params.set(CxParamType.PROJECT_NAME, "ast-cli-javascript-integration-success");
+    params.set(CxParamType.S, "./src");
+    params.set(CxParamType.FILTER, "*.ts,!**/node_modules/**/*");
+    params.set(CxParamType.BRANCH, "master");
+    const auth = new CxWrapper(new BaseTest());
+    const cxCommandOutput: CxCommandOutput = await auth.scanCreate(params);
+    const scan: CxScan = cxCommandOutput.payload.pop();
 
-        const cxProjectCommandOutput: CxCommandOutput = await auth.projectShow(scan.projectID);;
-        const size = cxProjectCommandOutput.payload.length;
-        expect(size).toBeGreaterThan(0);
-    });
+    const cxProjectCommandOutput: CxCommandOutput = await auth.projectShow(scan.projectID);;
+    const size = cxProjectCommandOutput.payload.length;
+    expect(size).toBeGreaterThan(0);
+});
 
-    it('ProjectBranches Successful case', async () => {
-        const params = new Map();
-        params.set(CxParamType.PROJECT_NAME, "ast-cli-javascript-integration-success");
-        params.set(CxParamType.S, "./src");
-        params.set(CxParamType.FILTER, "*.ts,!**/node_modules/**/*");
-        params.set(CxParamType.BRANCH, "master");
-        const auth = new CxWrapper(cxScanConfig);
-        const cxCommandOutput: CxCommandOutput = await auth.scanCreate(params);
-        const scan: CxScan = cxCommandOutput.payload.pop();
+it('ProjectBranches Successful case', async () => {
+    const params = new Map();
+    params.set(CxParamType.PROJECT_NAME, "ast-cli-javascript-integration-success");
+    params.set(CxParamType.S, "./src");
+    params.set(CxParamType.FILTER, "*.ts,!**/node_modules/**/*");
+    params.set(CxParamType.BRANCH, "master");
+    const auth = new CxWrapper(new BaseTest());
+    const cxCommandOutput: CxCommandOutput = await auth.scanCreate(params);
+    const scan: CxScan = cxCommandOutput.payload.pop();
 
-        const cxBranchCommandOutput: CxCommandOutput = await auth.projectBranches(scan.projectID,"");
-        expect(cxBranchCommandOutput.payload.length).toBeGreaterThan(0);
-    });
+    const cxBranchCommandOutput: CxCommandOutput = await auth.projectBranches(scan.projectID, "");
+    expect(cxBranchCommandOutput.payload.length).toBeGreaterThan(0);
 });
