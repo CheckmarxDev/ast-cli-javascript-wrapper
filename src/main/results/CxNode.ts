be2a66021f25039f5bb76c47a5a547e17e25f1d4
tiagobcx <tiago.baptista@checkmarx.com>
2022-06-27 10:08:39 +0100

    adding full results strucutre + support for new sca results

---
+++ src/main/results/CxNode.ts
@@ -0,0 +1,15 @@
+export default class CxNode {
+    id: string;
+    line: number;
+    name: string;
+    column: number;
+    length: number;
+    method: string;
+    nodeID: number;
+    domType: string;
+    fileName: string;
+    fullName: string;
+    typeName: string;
+    methodLine: number;
+    definitions: string;
+}
