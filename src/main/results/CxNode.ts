c3e20e23b2541249e5cdfd047357bd5688e758c2
tiagobcx <tiago.baptista@checkmarx.com>
2022-07-01 10:20:00 +0100

    adding test coverage

--- src/main/results/CxNode.ts
+++ src/main/results/CxNode.ts
@@ -12,4 +12,20 @@ export default class CxNode {
     typeName: string;
     methodLine: number;
     definitions: string;
+
+    constructor(id: string,line: number,name: string,column: number,length: number,method: string,nodeID: number,domType: string,fileName: string,fullName:string,typeName: string,methodLine: number,definitions: string) {
+        this.id = id;
+        this.line = line;
+        this.name = name;
+        this.column = column;
+        this.length = length;
+        this.method = method;
+        this.nodeID = nodeID;
+        this.domType = domType;
+        this.fileName = fileName;
+        this.fullName = fullName;
+        this.typeName = typeName;
+        this.methodLine = methodLine;
+        this.definitions = definitions;
+    }
 }
