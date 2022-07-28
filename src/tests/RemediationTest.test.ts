693f24ab8352839efca396a05c349361e05ae13e
tiagobcx <tiago.baptista@checkmarx.com>
2022-07-28 14:45:59 +0100

    trying a fix for tests

--- src/tests/RemediationTest.test.ts
+++ src/tests/RemediationTest.test.ts
@@ -9,7 +9,7 @@ describe("Kics Remediation cases",() => {
         const auth = new CxWrapper(cxScanConfig);
         auth.kicsRemediation("dist/tests/data/results.json",__dirname+"/dist/tests/data/","",).then((e:[Promise<CxCommandOutput>,any]) => {
             e[0].then((output:CxCommandOutput)=>{
-                const remediation: CxKicsRemediation = output.payload[0]
+                const remediation: CxKicsRemediation = output.payload[0];
                 expect(remediation.availableRemediation).toBeDefined();
                 expect(remediation.appliedRemediation).toBeDefined();
             })
@@ -18,9 +18,9 @@ describe("Kics Remediation cases",() => {
 
     it('Kics Remediation Successful case with filter', async () => {
         const auth = new CxWrapper(cxScanConfig);
-        auth.kicsRemediation("/Users/ricardobatista/Desktop/ast-cli/results.json",__dirname+"/dist/tests/data/","","9574288c118e8c87eea31b6f0b011295a39ec5e70d83fb70e839b8db4a99eba8").then((e:[Promise<CxCommandOutput>,any]) => {
+        auth.kicsRemediation("dist/tests/data/results.json",__dirname+"/dist/tests/data/","","9574288c118e8c87eea31b6f0b011295a39ec5e70d83fb70e839b8db4a99eba8").then((e:[Promise<CxCommandOutput>,any]) => {
             e[0].then((output)=>{
-                const remediation: CxKicsRemediation = output.payload[0]
+                const remediation: CxKicsRemediation = output.payload[0];
                 expect(remediation.availableRemediation).toBeDefined();
                 expect(remediation.appliedRemediation).toBeDefined();
             })
