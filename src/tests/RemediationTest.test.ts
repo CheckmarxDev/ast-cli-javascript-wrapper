f1331128d7f3eecc77c6ef9c76a188c80cf06200
tiagobcx <tiago.baptista@checkmarx.com>
2022-07-29 09:44:27 +0100

    trying a fix for tests

--- src/tests/RemediationTest.test.ts
+++ src/tests/RemediationTest.test.ts
@@ -7,7 +7,7 @@ describe("Kics Remediation cases",() => {
     const cxScanConfig = new BaseTest();
     it('Kics Remediation Successful case', async () => {
         const auth = new CxWrapper(cxScanConfig);
-        const e:[Promise<CxCommandOutput>,any] = await auth.kicsRemediation("dist/tests/data/results.json",__dirname+"/dist/tests/data","docker")
+        const e:[Promise<CxCommandOutput>,any] = await auth.kicsRemediation("dist/tests/data/results.json",__dirname+"/data","docker")
         const output = await e[0];
         const remediation: CxKicsRemediation = output.payload[0];
         expect(remediation.availableRemediation).toBeDefined();
@@ -16,7 +16,7 @@ describe("Kics Remediation cases",() => {
 
     it('Kics Remediation Successful case with filter', async () => {
         const auth = new CxWrapper(cxScanConfig);
-        const e:[Promise<CxCommandOutput>,any] = await auth.kicsRemediation("dist/tests/data/results.json",__dirname+"/dist/tests/data/","","9574288c118e8c87eea31b6f0b011295a39ec5e70d83fb70e839b8db4a99eba8")
+        const e:[Promise<CxCommandOutput>,any] = await auth.kicsRemediation("dist/tests/data/results.json",__dirname+"/data/","","9574288c118e8c87eea31b6f0b011295a39ec5e70d83fb70e839b8db4a99eba8")
         const output = await e[0];
         const remediation: CxKicsRemediation = output.payload[0];
         expect(remediation.availableRemediation).toBeDefined();
