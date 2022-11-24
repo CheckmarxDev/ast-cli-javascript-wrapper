5ccc1a72dacabe62d1873306abeeb842ddb98033
tiagobcx <tiago.baptista@checkmarx.com>
2022-11-24 10:40:13 +0000

    adding new direct dependency field

--- src/main/results/CxScaPackageData.ts
+++ src/main/results/CxScaPackageData.ts
@@ -7,13 +7,15 @@ export default class CxScaPackageData {
     outdated: boolean;
     fixLink:string
     supportsQuickFix:boolean;
+    isDirectDependency:boolean;
 
-    constructor(id: string,locations: string [],dependencyPaths: CxDependencyPaths [],outdated: boolean,fixLink:string,supportsQuickFix:boolean) {
+    constructor(id: string,locations: string [],dependencyPaths: CxDependencyPaths [],outdated: boolean,fixLink:string,supportsQuickFix:boolean,isDirectDependency:boolean) {
         this.id = id;
         this.locations = locations;
         this.dependencyPaths = dependencyPaths;
         this.outdated = outdated;
         this.fixLink = fixLink;
         this.supportsQuickFix = supportsQuickFix;
+        this.isDirectDependency = isDirectDependency;
     }
 }
