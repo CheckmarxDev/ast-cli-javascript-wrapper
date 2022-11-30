c131eb6d978961dac4de925e56e62d510f70dbf2
Tiago Baptista <92083272+tiagobcx@users.noreply.github.com>
2022-11-30 17:11:31 +0000

    adding new direct dependency field (#308)
    * adding new direct dependency field
    
    * using new cli field
    
    Co-authored-by: Pedro Lopes <83576881+pedrompflopes@users.noreply.github.com>

--- src/main/results/CxScaPackageData.ts
+++ src/main/results/CxScaPackageData.ts
@@ -7,13 +7,15 @@ export default class CxScaPackageData {
     outdated: boolean;
     fixLink:string
     supportsQuickFix:boolean;
+    typeOfDependency:string;
 
-    constructor(id: string,locations: string [],dependencyPaths: CxDependencyPaths [],outdated: boolean,fixLink:string,supportsQuickFix:boolean) {
+    constructor(id: string,locations: string [],dependencyPaths: CxDependencyPaths [],outdated: boolean,fixLink:string,supportsQuickFix:boolean,typeOfDependency:string) {
         this.id = id;
         this.locations = locations;
         this.dependencyPaths = dependencyPaths;
         this.outdated = outdated;
         this.fixLink = fixLink;
         this.supportsQuickFix = supportsQuickFix;
+        this.typeOfDependency = typeOfDependency;
     }
 }
