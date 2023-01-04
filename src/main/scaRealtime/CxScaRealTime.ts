8ddc16985d64a383576b18c392af30c2f16c1be2
hmmachadocx <hugo.machado@checkmarx.com>
2023-01-04 14:47:20 +0000

    AST-18457 - CLI | SCA Real-time | Create new command - Handle SCA Rest APIs
    Handle Sca Realtime

---
+++ src/main/scaRealtime/CxScaRealTime.ts
@@ -0,0 +1,12 @@
+export default class CxScaRealTime {
+    totalCount: string;
+    results: any = [];
+
+    static parseScaRealTimeResponse(resultObject: any): CxScaRealTime {
+        const scaRealTime: CxScaRealTime = new CxScaRealTime();
+        scaRealTime.totalCount = resultObject.totalCount;
+        scaRealTime.results = resultObject.results;
+
+        return scaRealTime;
+    }
+}
