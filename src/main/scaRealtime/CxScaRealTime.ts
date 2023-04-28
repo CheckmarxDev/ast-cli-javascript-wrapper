bf3b2cf6c4ab8ed484b57e1366f81e34ea7ab2de
Pedro Lopes <83576881+pedrompflopes@users.noreply.github.com>
2023-04-28 11:09:19 +0100

    Feature/sca realtime errors (#419)
    * add sca realtime errors
    
    * add sca realtime errors

--- src/main/scaRealtime/CxScaRealTime.ts
+++ src/main/scaRealtime/CxScaRealTime.ts
@@ -1,11 +1,14 @@
+import CxScaRealTimeErrors from "./CxScaRealTimeErrors";
+
 export default class CxScaRealTime {
     totalCount: string;
     results: any = [];
-
+    errors : CxScaRealTimeErrors[];
     static parseScaRealTimeResponse(resultObject: any): CxScaRealTime {
         const scaRealTime: CxScaRealTime = new CxScaRealTime();
         scaRealTime.totalCount = resultObject.totalCount;
         scaRealTime.results = resultObject.results || [];
+        scaRealTime.errors = resultObject.errors.map((errorObject: any) => CxScaRealTimeErrors.parseScaRealTimeResponseError(errorObject)) || [];
 
         return scaRealTime;
     }
