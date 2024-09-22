ae35e165c407c8746e018f69d081704ca3cdb5b0
AlvoBen <alvo@post.bgu.ac.il>
2024-09-22 11:06:58 +0300

    refactor

--- src/main/osinstaller/CxInstaller.ts
+++ src/main/osinstaller/CxInstaller.ts
@@ -1,17 +1,19 @@
 import * as fs from 'fs/promises';
+import * as fs1 from "fs"
 import * as path from 'path';
-import AdmZip from 'adm-zip'; // For extracting ZIP files
 import * as tar from 'tar';
 import axios from 'axios';
-import {createWriteStream} from "node:fs";
+import * as unzipper from "unzipper";
 
 
 export class CxInstaller {
     private readonly platform: string;
     private cliVersion: string;
+    private readonly resourceDirPath: string;
 
     constructor(platform: string) {
         this.platform = platform;
+        this.resourceDirPath = path.join(__dirname, `../wrapper/resources`);
     }
 
     // Method to get the download URL based on OS and architecture
@@ -42,119 +44,73 @@ export class CxInstaller {
     
     getExecutablePath(): string {
         let executablePath;
-        const dirExecutablePath = path.join(__dirname, `../wrapper/resources`);
         if (this.platform === 'win32') {
-            executablePath = path.join(dirExecutablePath, 'cx.exe');
+            executablePath = path.join(this.resourceDirPath, 'cx.exe');
         } else {
-            executablePath = path.join(dirExecutablePath, 'cx');
+            executablePath = path.join(this.resourceDirPath, 'cx');
         }
         return executablePath;
+    }
+
+    async downloadIfNotInstalledCLI() {
+        if (!this.checkExecutableExists()) {
+            const url = await this.getDownloadURL();
+            const zipPath = this.getZipPath();
+            try {
+                await this.downloadFile(url, zipPath);
+                console.log('Downloaded CLI to:', zipPath);
+
+                await this.extractArchive(zipPath, this.resourceDirPath);
+                console.log('Extracted CLI to:', this.resourceDirPath);
+                console.log('Done!');
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        }
+    }
+
+    async extractArchive(zipPath: string, extractPath: string): Promise<void> {
+        if (zipPath.endsWith('.zip')) {
+            console.log('Extracting ZIP file...');
+            // Use unzipper to extract ZIP files
+            await unzipper.Open.file(zipPath)
+                .then(d => d.extract({path: extractPath}));
+            console.log('Extracted ZIP file to:', extractPath);
+        } else if (zipPath.endsWith('.tar.gz')) {
+            console.log('Extracting TAR.GZ file...');
+            // Use tar.extract to extract TAR.GZ files
+            await tar.extract({file: zipPath, cwd: extractPath});
+            console.log('Extracted TAR.GZ file to:', extractPath);
+        } else {
+            console.error('Unsupported file type. Only .zip and .tar.gz are supported.');
+        }
+    }
+
+    async downloadFile(url: string, outputPath: string) {
+        const writer = fs1.createWriteStream(outputPath);
+        const response = await axios({url, responseType: 'stream'});
+        response.data.pipe(writer);
+        return new Promise((resolve, reject) => {
+            writer.on('finish', resolve);
+            writer.on('error', reject);
+        });
     }
 
     getZipPath(): string {
         let executablePath;
-        const dirExecutablePath = path.join(__dirname, `../wrapper/resources/`);
         if (this.platform === 'win32') {
-            executablePath = path.join(dirExecutablePath, 'cx.zip');
+            executablePath = path.join(this.resourceDirPath, 'cx.zip');
         } else {
-            executablePath = path.join(dirExecutablePath, 'cx.tar.gz');
+            executablePath = path.join(this.resourceDirPath, 'cx.tar.gz');
         }
         return executablePath;
     }
 
-    async getCLIExecutableName(): Promise<string> {
-        let platformString: string;
-        let archiveExtension: string;
-        this.cliVersion = await this.readASTCLIVersion();
-
-        switch (this.platform) {
-            case 'win32':
-                platformString = 'windows';
-                archiveExtension = 'zip';
-                break;
-            case 'darwin':
-                archiveExtension = 'tar.gz';
-                platformString = 'darwin';
-                break;
-            case 'linux':
-                archiveExtension = 'tar.gz';
-                platformString = 'linux';
-                break;
-            default:
-                throw new Error('Unsupported platform or architecture');
-        }
-
-        return `ast-cli_${this.cliVersion}_${platformString}_x64.${archiveExtension}`;
-    }
-
-    removeExtension(fileName: string): string {
-        if (fileName.endsWith('.tar.gz')) {
-            return fileName.slice(0, -7); // Remove '.tar.gz'
-        }
-        return fileName.replace(/\.[^/.]+$/, ''); // Remove other extensions like '.zip'
-    }
-
-    
-
-    // Method to extract the file (ZIP or tar.gz)
-    async extractFile(filePath: string, outputDir: string): Promise<void> {
-        if (filePath.endsWith('.zip')) {
-            // Extract ZIP file
-            const zip = new AdmZip(filePath);
-            zip.extractAllTo(outputDir, true); // Extract to outputDir
-            console.log(`Extracted ZIP to ${outputDir}`);
-        } else if (filePath.endsWith('.tar.gz')) {
-            // Extract tar.gz file
-            await tar.extract({
-                file: filePath,
-                cwd: outputDir, // Extract to the outputDir
-            });
-            console.log(`Extracted tar.gz to ${outputDir}`);
-        } else {
-            throw new Error('Unsupported archive format');
-        }
-    }
-
-    // Method to execute the installation
-    async install(outputPath: string): Promise<void> {
-        const exists = await this.checkExecutableExists();
-        if (exists) {
-            console.log('Executable already exists. Skipping installation.');
-            return;
-        }
-
-        const url = await this.getDownloadURL();
-        if (!url) {
-            console.error('No valid download URL available for this platform.');
-            return;
-        }
-        
-        try {
-            console.log(`Downloading from: ${url}`);
-            await downloadFile(url, outputPath);
-            console.log(`Downloaded to: ${outputPath}`);
-
-            // Now extract the downloaded archive
-        } catch (error) {
-            console.error(`Error during installation: ${error.message}`);
-        }
-    }
-
-    // Check if the executable exists
-    async checkExecutableExists(): Promise<boolean> {
-        let executablePath;
-        const dirExecutablePath = path.join(__dirname, `../../wrapper/resources/`);
-        if (this.platform === 'win32') {
-            executablePath = path.join(dirExecutablePath, 'cx.exe');
-        } else {
-            executablePath = path.join(dirExecutablePath, 'cx');
-        }
-        try {
-            await fs.access(executablePath);
-            console.log(`Executable exists at: ${executablePath}`);
+    checkExecutableExists(): boolean {
+        if (fs1.existsSync(this.getExecutablePath())){
+            console.log('Executable exists:', this.getExecutablePath());
             return true;
-        } catch (error) {
-            console.error(`Executable does not exist at: ${executablePath}`);
+        } else {
             return false;
         }
     }
@@ -172,52 +128,6 @@ export class CxInstaller {
             console.error('Error reading AST CLI version:', error);
             throw error;
         }
-    }
-}
-
-async function downloadFile(downloadURLPath: string, filePath: string): Promise<void> {
-    const fileName = "cx";
-    console.log(`Downloading ${fileName} from: ${downloadURLPath}`);
-
-    try {
-        // Ensure the directory exists
-        await fs.mkdir(path.dirname(downloadURLPath), { recursive: true });
-
-        // Check if filePath is a directory
-        try {
-            const stats = await fs.stat(filePath);
-            if (stats.isDirectory()) {
-                // If it's a directory, append the filename from the URL
-                filePath = path.join(filePath, path.basename(downloadURLPath));
-            }
-        } catch (error) {
-            // If the path doesn't exist, assume it's meant to be a file
-            // The directory has already been created above
-        }
-
-        // Perform HTTP GET request
-        const response = await axios({
-            method: 'GET',
-            url: downloadURLPath,
-            responseType: 'stream'
-        });
-        // Create the file stream at the specified filePath
-        const fileStream = createWriteStream(process.cwd()+"/src/main/wrapper/resources/cx");
-
-        // Pipe the response data to the file
-        response.data.pipe(fileStream);
-
-        // Wait for the file to finish writing
-        await new Promise<void>((resolve, reject) => {
-            fileStream.on('finish', resolve);
-            fileStream.on('error', reject);
-        });
-
-        console.log(`File downloaded successfully to ${filePath}`);
-
-    } catch (error) {
-        console.log(`Error during file download:` + error.message);
-        throw new Error(`Invoking HTTP request to download file failed - ${error.message}`);
     }
 }
 
