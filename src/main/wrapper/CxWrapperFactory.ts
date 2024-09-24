6c37f42f54f1bc18627f377a346ade9f152782d4
AlvoBen <alvo@post.bgu.ac.il>
2024-09-24 15:46:20 +0300

    make CxWrapper constructor private

--- src/main/wrapper/CxWrapperFactory.ts
+++ src/main/wrapper/CxWrapperFactory.ts
@@ -4,11 +4,9 @@ import {CxConfig} from "./CxConfig";
 class CxWrapperFactory {
     static async createWrapper(cxScanConfig: CxConfig, logFilePath?: string, type?: string): Promise<CxWrapper> {
         let wrapper: CxWrapper;
-
+        wrapper = await CxWrapper.getInstance(cxScanConfig, logFilePath);
         if (type === 'mock') {
-            wrapper = new CxWrapper(cxScanConfig, logFilePath);
-        } else {
-            wrapper = await CxWrapper.getInstance(cxScanConfig, logFilePath);
+            wrapper = wrapper.cloneWithNewConfig(cxScanConfig);
         }
         await wrapper.init();
         return wrapper;
