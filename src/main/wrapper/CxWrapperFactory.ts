0553d9ef3cb0744ad12d2d20c161e6bd14f701a8
AlvoBen <alvo@post.bgu.ac.il>
2024-09-24 16:39:46 +0300

    added "multiple singleton" - one for each different scan configuration and log file path

--- src/main/wrapper/CxWrapperFactory.ts
+++ src/main/wrapper/CxWrapperFactory.ts
@@ -2,12 +2,9 @@ import {CxWrapper} from "./CxWrapper";
 import {CxConfig} from "./CxConfig";
 
 class CxWrapperFactory {
-    static async createWrapper(cxScanConfig: CxConfig, logFilePath?: string, type?: string): Promise<CxWrapper> {
+    static async createWrapper(cxScanConfig: CxConfig, logFilePath?: string): Promise<CxWrapper> {
         let wrapper: CxWrapper;
         wrapper = await CxWrapper.getInstance(cxScanConfig, logFilePath);
-        if (type === 'mock') {
-            wrapper = wrapper.cloneWithNewConfig(cxScanConfig);
-        }
         await wrapper.init();
         return wrapper;
     }
