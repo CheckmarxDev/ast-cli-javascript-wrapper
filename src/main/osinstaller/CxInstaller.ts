1324f8e620bac1d1e2d92ceb27936c35aa2f73a2
AlvoBen <alvo@post.bgu.ac.il>
2024-11-24 12:38:20 +0200

    check

--- src/main/osinstaller/CxInstaller.ts
+++ src/main/osinstaller/CxInstaller.ts
@@ -98,11 +98,7 @@ export class CxInstaller {
             fs.chmodSync(this.getExecutablePath(), 0o755);
             logger.info('Extracted CLI to:', this.resourceDirPath);
         } catch (error) {
-            if (await this.checkLatestExecutableVersionIsInstalled()) {
-                return;
-            }
             logger.error('Error during installation:', error);
-            process.exit(1);
         }
     }
 
