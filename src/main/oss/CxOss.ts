d16fc25833b63197e909b478e4b3af10c9de68f2
cx-sarah-chen <173361628+cx-sarah-chen@users.noreply.github.com>
2025-05-20 14:24:57 +0300

    Refactors OSS scan parsing and renames method

--- src/main/oss/CxOss.ts
+++ src/main/oss/CxOss.ts
@@ -13,32 +13,33 @@ export default class CxOssResult {
 
 
   static parseResult(resultObject: any): CxOssResult[] {
+    let packages = resultObject.Packages;
     let ossResults: CxOssResult[] = [];
-    if (resultObject instanceof Array) {
-        ossResults = resultObject.map((member: any) => {
+    if (packages instanceof Array) {
+        ossResults = packages.map((member: any) => {
         const ossResult = new CxOssResult();
         ossResult.packageManager = member.PackageManager;
-        ossResult.packageName = member.PackageName;
-        ossResult.version = member.Version;
-        ossResult.filepath = member.Filepath;
-        ossResult.lineStart = member.LineStart;
-        ossResult.lineEnd = member.LineEnd;
-        ossResult.startIndex = member.StartIndex;
-        ossResult.endIndex = member.EndIndex;
-        ossResult.status = member.Status as CxManifestStatus;
+                ossResult.packageName = member.PackageName;
+                ossResult.version = member.PackageVersion;
+                ossResult.filepath = member.FilePath;
+                ossResult.lineStart = member.LineStart;
+                ossResult.lineEnd = member.LineEnd;
+                ossResult.startIndex = member.StartIndex;
+                ossResult.endIndex = member.EndIndex;
+                ossResult.status = member.Status as CxManifestStatus;
         return ossResult;
       });
     } else {
         const ossResult = new CxOssResult();
-        ossResult.packageManager = resultObject.PackageManager;
-        ossResult.packageName = resultObject.PackageName;
-        ossResult.version = resultObject.Version;
-        ossResult.filepath = resultObject.FilePath;
-        ossResult.lineStart = resultObject.LineStart;
-        ossResult.lineEnd = resultObject.LineEnd;
-        ossResult.startIndex = resultObject.StartIndex;
-        ossResult.endIndex = resultObject.EndIndex; 
-        ossResult.status = resultObject.Status as CxManifestStatus;
+        ossResult.packageManager = packages.PackageManager;
+        ossResult.packageName = packages.PackageName;
+        ossResult.version = packages.PackageVersion;
+        ossResult.filepath = packages.FilePath;
+        ossResult.lineStart = packages.LineStart;
+        ossResult.lineEnd = packages.LineEnd;
+        ossResult.startIndex = packages.StartIndex;
+        ossResult.endIndex = packages.EndIndex; 
+        ossResult.status = packages.Status as CxManifestStatus;
         ossResults.push(ossResult);
     }
     return ossResults;
